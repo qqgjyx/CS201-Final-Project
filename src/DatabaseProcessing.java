2caff48196a6612516692ef2751bc4501ab4bf63
qqgjyx <jw853@duke.edu>
2023-12-07 16:10:04 +0800

    Frameworks setted

---
+++ src/DatabaseProcessing.java
@@ -0,0 +1,23 @@
+public class DatabaseProcessing {
+}
+
+class MyBST {
+//    TODO
+//    int getInfo(){}
+//    void insert(){}
+//    LIst<E> search(E e){}
+}
+
+class MyHashmap {
+}
+
+class MyHeap {
+}
+
+//TODO
+class PeopleRecord {
+
+}
+
+
+
